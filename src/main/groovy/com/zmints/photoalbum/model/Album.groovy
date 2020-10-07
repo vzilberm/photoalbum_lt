cd06e69c7b8de345f4b54d097778324856946039
vzilberm <vzilbermints@gmail.com>
2020-10-07 14:11:37 -0500

    Unirest problems

--- src/main/groovy/com/zmints/photoalbum/model/Album.groovy
+++ src/main/groovy/com/zmints/photoalbum/model/Album.groovy
@@ -1,4 +1,17 @@
 package com.zmints.photoalbum.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 class Album {
+
+    @JsonProperty("albumId")
+    int albumId
+    @JsonProperty("id")
+    int id
+    @JsonProperty("title")
+    String title
+    @JsonProperty("url")
+    String url
+    @JsonProperty("thumbnailUrl")
+    String thumbnailUrl
 }
--- src/main/groovy/com/zmints/photoalbum/repository/PhotoAlbumRepository.groovy
+++ src/main/groovy/com/zmints/photoalbum/repository/PhotoAlbumRepository.groovy
@@ -1,4 +1,29 @@
 package com.zmints.photoalbum.repository
 
-class PhotoalbumRepository {
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest
+import com.zmints.photoalbum.model.Album;
+import org.springframework.stereotype.Component;
+
+import java.net.http.HttpResponse
+
+class PhotoAlbumRepository {
+    List<Album> findAll() {
+        try {
+
+            HttpResponse<JsonNode> jsonResponse = Unirest.get("https://jsonplaceholder.typicode.com/photos").header("accept", "application/json").asJson();
+            JsonNode result = jsonResponse.getBody();
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readValue(result.toString(), new TypeReference<List<Album>>() {});
+
+        } catch (Exception e) {
+            System.out.println("Some Error Has Occured");
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
