e91a74efe1ecafa424e2d2ed4bca39a9d0f015d0
vzilberm <vzilbermints@gmail.com>
2020-10-07 15:54:12 -0500

    Viktoriya: added repository to extract photo albums and unit test for it

--- src/main/groovy/com/zmints/photoalbum/repository/PhotoAlbumRepository.groovy
+++ src/main/groovy/com/zmints/photoalbum/repository/PhotoAlbumRepository.groovy
@@ -1,24 +1,47 @@
 package com.zmints.photoalbum.repository
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest
-import com.zmints.photoalbum.model.Album;
-import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.zmints.photoalbum.model.Album
+import groovy.util.logging.Slf4j
+import kong.unirest.HttpResponse
+import kong.unirest.JsonNode
+import kong.unirest.Unirest
+import org.springframework.stereotype.Component
 
-import java.net.http.HttpResponse
-
+@Slf4j
+@Component
 class PhotoAlbumRepository {
+
     List<Album> findAll() {
         try {
 
-            HttpResponse<JsonNode> jsonResponse = Unirest.get("https://jsonplaceholder.typicode.com/photos").header("accept", "application/json").asJson();
-            JsonNode result = jsonResponse.getBody();
-            ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.readValue(result.toString(), new TypeReference<List<Album>>() {});
+            HttpResponse<JsonNode> jsonResponse =
+                    Unirest.get("https://jsonplaceholder.typicode.com/photos")
+                            .header("accept", "application/json")
+                            .asJson()
 
+            JsonNode result = jsonResponse.body
+            ObjectMapper objectMapper = new ObjectMapper()
+            return objectMapper.readValue(result.toString(), new TypeReference<List<Album>>() {})
+
+        } catch (Exception e) {
+            log.info("Something went wrong", e)
+        }
+
+        return null
+    }
+
+    List<Album> findById(Integer id) {
+        try {
+            HttpResponse<JsonNode> jsonResponse =
+                    Unirest.get("https://jsonplaceholder.typicode.com/photos")
+                            .queryString("albumId", id)
+                            .header("accept", "application/json")
+                            .asJson()
+            JsonNode result = jsonResponse.body
+            ObjectMapper objectMapper = new ObjectMapper()
+            return objectMapper.readValue(result.toString(), new TypeReference<List<Album>>() {})
         } catch (Exception e) {
             System.out.println("Some Error Has Occured");
             e.printStackTrace();
---
+++ src/test/groovy/com/zmints/photoalbum/repository/PhotoAlbumRepositoryTest.groovy
@@ -0,0 +1,41 @@
+package com.zmints.photoalbum.repository
+
+import com.zmints.photoalbum.model.Album
+import org.junit.Before
+import org.junit.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+class PhotoAlbumRepositoryTest {
+    PhotoAlbumRepository repository
+
+    @Before
+    void setUp() {
+        repository = new PhotoAlbumRepository()
+    }
+
+    @Test
+    void findAll() {
+        List<Album> albums = repository.findAll()
+        assert albums.size() > 0
+        assert albums.first().getId() == 1
+        assert albums.first().getTitle().equals("accusamus beatae ad facilis cum similique qui sunt")
+        assert albums.first().getAlbumId() == 1
+        assert albums.first().getThumbnailUrl().equals("https://via.placeholder.com/150/92c952")
+    }
+
+    @Test
+    void findById() {
+        List<Album> albums = repository.findById(1)
+        assert albums.size() > 0
+        assert albums.first().albumId == 1
+        assert albums.last().albumId == 1
+
+    }
+
+    @Test
+    void no_albums_found(){
+        List<Album> photoAlbumList = repository.findById(999)
+        assert photoAlbumList.size() == 0
+    }
+}
+
