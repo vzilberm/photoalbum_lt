d4d538f95dcb4254182ffd463d7582a2bddd62e7
vzilberm <vzilbermints@gmail.com>
2020-10-07 11:34:14 -0500

    Viktoriya: initial commit

---
+++ src/main/groovy/com/zmints/photoalbum/PhotoalbumApplication.groovy
@@ -0,0 +1,13 @@
+package com.zmints.photoalbum
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PhotoalbumApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PhotoalbumApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/zmints/photoalbum/PhotoalbumApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.zmints.photoalbum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PhotoalbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
