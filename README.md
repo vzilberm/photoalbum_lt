8ac0894e162c8ef91723e185e5a6b451502473eb
vzilberm <vzilbermints@gmail.com>
2020-10-07 21:52:39 -0500

    Viktoriya: calling PhotoAlbumService from PhotoalbumApplication class. Added README file.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# photoalbum_lt
+# Photo Album Assignment
+
+List the id and the title of the photo albums
+The albums json provided at https://jsonplaceholder.typicode.com/photos.
+
+## Installation
+
+Clone from github:
+
+    git clone https://github.com/vzilberm/photoalbum_lt
+
+After downloading project from github, run a Maven build:
+
+    mvn clean install
+
+
+When build is successful, a target/ folder will be created.
+It will contain the built photoalbum-0.0.1-SNAPSHOT.jar file. 
+
+## to run application:
+Change dir to /target.
+Run the Java JAR file with arguments ("photoalbum {albumId}").
+It will output the results in the console environment.
+
+    java -jar photoalbum-0.0.1-SNAPSHOT.jar photoalbum 1
+
--- src/main/groovy/com/zmints/photoalbum/PhotoalbumApplication.groovy
+++ src/main/groovy/com/zmints/photoalbum/PhotoalbumApplication.groovy
@@ -1,13 +1,36 @@
 package com.zmints.photoalbum
 
+import com.zmints.photoalbum.service.PhotoAlbumService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class PhotoalbumApplication {
-
+class PhotoalbumApplication  implements CommandLineRunner {
+	@Autowired
+	PhotoAlbumService photoAlbumService
 	static void main(String[] args) {
 		SpringApplication.run(PhotoalbumApplication, args)
 	}
 
+	@Override
+	void run(String... args) {
+		if (args.length == 0) return
+		Integer albumId = null
+
+		try {
+			albumId = Integer.parseInt(args[1]);
+		}  catch(Exception e) {
+			System.out.println("Invalid Argument, please try again.")
+
+		}
+
+		List<String> result = photoAlbumService.listByAlbumId(albumId)
+		result.each {
+			System.out.println(it)
+		}
+
+		System.exit(0)
+	}
 }
